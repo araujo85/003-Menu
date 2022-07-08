4b37eef52beaa377bbf85e36c400550c0ca5d0f8
araujo85 <108906505+araujo85@users.noreply.github.com>
2022-07-08 10:48:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const boxList = document.querySelector(".boxList")
+const allLi = document.querySelectorAll("li")
+
+allLi.forEach( (li, index) => {
+
+    li.addEventListener("click", e => {
+        boxList.querySelector(".active-list").classList.remove("active-list")
+        li.classList.add("active-list")
+
+        const indicator = document.querySelector(".indicator")
+        indicator.style.transform = `translateX(calc(${index * 90}px))`
+    })
+})
